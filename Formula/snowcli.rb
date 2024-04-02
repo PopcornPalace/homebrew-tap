2d780ed83ac83d272e8dd5c1f7f0f21fb92157b6
Valentyn Nastenko <valentin.nastenko@doublegood.com>
2024-04-02 13:24:02 +0200

    add snowflake cli

---
+++ Formula/snowcli.rb
@@ -0,0 +1,23 @@
+  class Snowcli < Formula
+    include Language::Python::Virtualenv
+    desc "A CLI for Snowflake development"
+    homepage "https://github.com/snowflake-labs/snowflake-cli"
+    url "https://files.pythonhosted.org/packages/0f/39/7ac3590bca2eaa3fa079339733988e1d3ef1143aa6e40032dbcf5f641be6/snowflake_cli_labs-2.1.2.tar.gz"
+    sha256 "23eb0023c9d38c9e8ae02cfc1adcae757b5d068641a43b63b0b25dd4bfdf0bc0"
+
+    depends_on "python3"
+
+    def install
+      ENV["CARGO_NET_GIT_FETCH_WITH_CLI"] = "true"
+      venv = virtualenv_create(libexec, "python3", system_site_packages: false)
+      venv.instance_variable_get(:@formula).system venv.instance_variable_get(:@venv_root)/"bin/python",
+        "-m", "ensurepip", "--upgrade"
+      venv.instance_variable_get(:@formula).system venv.instance_variable_get(:@venv_root)/"bin/python",
+        "-m", "pip", "install", "snowflake-cli-labs==2.1.2"
+      bin.install_symlink "#{libexec}/bin/snow" => "snow"
+    end
+
+    test do
+      false
+    end
+  end
